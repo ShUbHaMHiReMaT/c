0becb2317282b7e36ae77091c532ad7b939b0159
ShUbHaMHiReMaT <shubhamhiremath87@gmail.com>
2026-09-04 16:55:19 +0530

    this is new one

---
+++ c/patternmaking.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n;
+    int i,j;
+    scanf("%d", &n);
+    for(i=0;i<=n;i++){
+        for(j=0;j<=n;j++){
+            printf("%d\n",n);
+        }
+        
+    }
+  	// Complete the code to print the pattern.
+    return 0;
+}
